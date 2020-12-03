c85b199c37f8ab1554cc378a519fe1aa39e4d78d
KUNIHIKO-MIZUNO <mjapan731@gmail.com>
2020-12-03 17:19:30 +0900

    新規登録・ログイン・ログアウト　動作確認

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname,         presence: true
   validates :birthday,         presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
     validates :last_name,        presence: true
     validates :first_name,       presence: true
     validates :last_name_kana,   presence: true
@@ -15,6 +15,6 @@ class User < ApplicationRecord
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: '英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX
 
 end
