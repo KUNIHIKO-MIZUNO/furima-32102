546b31829b0be00142005f4994334cd95cd991c4
KUNIHIKO-MIZUNO <mjapan731@gmail.com>
2020-12-04 15:59:12 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
   validates :birthday,         presence: true
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
-    validates :last_name,        presence: true
-    validates :first_name,       presence: true
+    validates :last_name
+    validates :first_name
   end
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width katakana characters' } do
-    validates :last_name_kana,   presence: true
-    validates :first_name_kana,  presence: true
+    validates :last_name_kana
+    validates :first_name_kana
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,10 @@ describe User do
         @user.email = 'a@a'
         expect(@user).to be_valid
       end
+      it "全ての情報が正しい場合登録できる" do
+        expect(@user).to be_valid
+      end
     end
-
     context '新規登録がうまくいかないとき' do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
@@ -72,27 +74,37 @@ describe User do
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
       it 'passwordが半角英数字混合でなければ登録できない' do
-        @user.password = "/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i"
+        @user.password = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+      it 'passwordが英語のみでは登録できない' do
+        @user.password = "aaaaaa"
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+      end
+      it 'passwordが数字のみでは登録できない' do
+        @user.password = "111111"
         @user.valid?
         expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
       end
       it 'last_nameが全角でなければ登録できない' do
-        @user.last_name = "/\A[ぁ-んァ-ン一-龥]+\z/"
+        @user.last_name = "aaaaaa"
         @user.valid?
         expect(@user.errors.full_messages).to include('Last name Full-width characters')
       end
       it 'first_nameが全角でなければ登録できない' do
-        @user.first_name = "/\A[ぁ-んァ-ン一-龥]+\z/"
+        @user.first_name = "aaaaaa"
         @user.valid?
         expect(@user.errors.full_messages).to include('First name Full-width characters')
       end
       it 'last_name_kanaが全角でなければ登録できない' do
-        @user.last_name_kana = "/\A[ぁ-んァ-ン一-龥]+\z/"
+        @user.last_name_kana = "aaaaaa"
         @user.valid?
         expect(@user.errors.full_messages).to include('Last name kana Full-width katakana characters')
       end
       it 'first_name_kanaが全角でなければ登録できない' do
-        @user.first_name_kana = "/\A[ぁ-んァ-ン一-龥]+\z/"
+        @user.first_name_kana = "aaaaaa"
         @user.valid?
         expect(@user.errors.full_messages).to include('First name kana Full-width katakana characters')
       end
