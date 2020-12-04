7df483ce7b0cbdcc91cb795a1fab4d99f4ff60d5
KUNIHIKO-MIZUNO <mjapan731@gmail.com>
2020-12-04 14:42:36 +0900

    Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,15 @@ class User < ApplicationRecord
   validates :nickname,         presence: true
   validates :birthday,         presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters'} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
     validates :last_name,        presence: true
     validates :first_name,       presence: true
   end
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width katakana characters'} do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width katakana characters' } do
     validates :last_name_kana,   presence: true
     validates :first_name_kana,  presence: true
   end
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {"1a" + Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    birthday              {Faker::Date.birthday}
-    last_name             {Gimei.unique.last}
-    first_name            {Gimei.unique.first}
-    last_name_kana        {Gimei.last.katakana}
-    first_name_kana       {Gimei.first.katakana}
-
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    birthday              { Faker::Date.birthday }
+    last_name             { Gimei.unique.last }
+    first_name            { Gimei.unique.first }
+    last_name_kana        { Gimei.last.katakana }
+    first_name_kana       { Gimei.first.katakana }
   end
 end
