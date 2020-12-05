42c52e3afeaa5a3573c42a0ef6db4f3c70aee046
KUNIHIKO-MIZUNO <mjapan731@gmail.com>
2020-12-05 17:36:05 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,21 @@ class Item < ApplicationRecord
   belongs_to :delivery_cost
   belongs_to :prefecture
   belongs_to :delivery_how_long
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :explain
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_cost_id
+    validates :prefecture_id
+    validates :delivery_how_long_id
+  end
+
+  validates :price, numericality:{ greater_than_or_equal_to:300, less_than_or_equal_to: 9,999,999, message: 'Price Out of setting range'}    
+
 end
