d24d9f9f1944349a3af5cb7c2ac26d9702c589dd
KUNIHIKO-MIZUNO <mjapan731@gmail.com>
2020-12-05 15:46:27 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchases
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_cost
+  belongs_to :prefecture
+  belongs_to :delivery_how_long
 end
---
+++ spec/factories/delivery_how_longs.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :delivery_how_long do
+    
+  end
+end
