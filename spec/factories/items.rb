7344378f041c4adc816006de1f15c097e0a93759
KUNIHIKO-MIZUNO <mjapan731@gmail.com>
2020-12-07 17:36:42 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name {'あ'}
+    explain {'あ'}
+    price {1000}
+    category_id {2}
+    condition_id {2}
+    delivery_cost_id {2}
+    prefecture_id {2}
+    delivery_how_long_id {2}
+
+    association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/apple-touch-icon.png'), filename: 'apple-touch-icon.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,62 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品登録' do
+    context '商品登録がうまくいくとき' do
+      it "全ての情報が正しい場合登録できる" do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品登録がうまくいかないとき' do
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'item_nameが空では登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'explainが空では登録できない' do
+        @item.explain = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explain can't be blank")
+      end
+      it 'priceが¥300~¥9,999,999の間では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price Price Out of setting range")
+      end
+      it 'category_idが「---」では登録できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'condition_idが「---」では登録できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+      end
+      it 'delivery_cost_idが「---」では登録できない' do
+        @item.delivery_cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery cost must be other than 1")
+      end
+      it 'prefecture_idが「---」では登録できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it 'delivery_how_long_idが「---」では登録できない' do
+        @item.delivery_how_long_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery how long must be other than 1")
+      end
+    end
+  end
 end
