fe1f132d780817eebd13bfcaded06b309fcd3bfb
KUNIHIKO-MIZUNO <mjapan731@gmail.com>
2020-12-08 17:50:15 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,11 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @items = Item.all
   end
-
   def new
     @item = Item.new
   end
-
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -16,9 +15,11 @@ class ItemsController < ApplicationController
     end
   end
 
+
   private
 
   def item_params
     params.require(:item).permit(:item_name, :price, :explain, :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :delivery_how_long_id, :image).merge(user_id: current_user.id)
   end
 end
+
