49015132dc60d0131c932b05734bb2a9af5c8a37
KUNIHIKO-MIZUNO <mjapan731@gmail.com>
2020-12-19 16:59:51 +0900

    挙動確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
-    @items = Item.includes(:user).order('created_at ASC')
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
